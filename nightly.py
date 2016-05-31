wikimedia/labs-tools-extdist

commit bb346ce77399412801f40ce9771e3ca2e06c52c4

Generate a gitinfo.json to be included in tarballs
Bug: T122769
Change-Id: I4573f6c9ce3f0e95bfe447c6396d0aa59a0d3ee3

nightly.py
@@ -183,6 +183,26 @@ class TarballGenerator(object):
                 except subprocess.CalledProcessError:
                     logging.error(traceback.format_exc())
                     logging.error('composer install failed')
+            # Create gitinfo.json to be read/displayed by Special:Version
+            git_info = {}
+            with open('.git/HEAD') as f_head:
+                head = f_head.read()
+            if head.startswith('ref:'):
+                head = head[5:]  # Strip 'ref :'
+            git_info['head'] = head
+            # Get the SHA-1
+            git_info['headSHA1'] = self.shell_exec(['git', 'rev-parse', 'HEAD'])
+            git_info['headCommitDate'] = self.shell_exec(['git', 'show', '-s', '--format=format:%ct', 'HEAD'])
+            if head.startswith('refs/heads'):
+                branch = head.split('/')[-1]
+            else:
+                branch = head
+            git_info['branch'] = branch
+            git_info['remoteURL'] = self.GIT_URL % ext
+            with open('gitinfo.json', 'w') as f:
+                json.dump(git_info, f)
+
+            # TODO: Stop writing this file now that we have gitinfo.json
             # Create a 'version' file with basic info about the tarball
             with open('version', 'w') as f:
                 f.write('%s: %s\n' % (ext, branch))

commit caa01457476182d240bc368274ba1cb32565b1d3

Don't overload the `branch` variable
Fixes a regression from bb346ce7739941, which broke the deletion of
master tarballs.

Bug: T123180
Change-Id: I2ec970eaf69f78c3305b8dffeba972881780ca69

nightly.py
@@ -197,10 +197,10 @@ class TarballGenerator(object):
             git_info['headSHA1'] = self.shell_exec(['git', 'rev-parse', 'HEAD'])
             git_info['headCommitDate'] = self.shell_exec(['git', 'show', '-s', '--format=format:%ct', 'HEAD'])
             if head.startswith('refs/heads'):
-                branch = head.split('/')[-1]
+                gi_branch = head.split('/')[-1]
             else:
-                branch = head
-            git_info['branch'] = branch
+                gi_branch = head
+            git_info['branch'] = gi_branch
             git_info['remoteURL'] = self.GIT_URL % ext
             with open('gitinfo.json', 'w') as f:
                 json.dump(git_info, f)